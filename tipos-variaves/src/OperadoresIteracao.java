18a927211490e25c80efcb8e51ef57b84d7cc886
Murilo <murilogamerbr72@gmail.com>
2025-04-28 14:45:52 -0300

    Update 1.3 Operadores Iteração

---
+++ tipos-variaves/src/OperadoresIteracao.java
@@ -0,0 +1,18 @@
+public class OperadoresIteracao {
+    public static void main(String[] args) {
+        int numero = 5;
+        //x Repetição
+
+        //numero = numero + 2;
+        
+        numero++; // também pode decrementar com --
+        System.out.println(numero);
+
+        //negar expressão boolean
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+        System.out.println(variavel);
+    }
+}
